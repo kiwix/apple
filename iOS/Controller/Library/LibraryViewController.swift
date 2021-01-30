cfd947549cab15b5d17970788941393d2bc1b669
Chris Li <chris@kiwix.org>
2021-01-30 14:48:53 -0500

    LibraryViewController

--- iOS/Controller/Library/LibraryViewController.swift
+++ iOS/Controller/Library/LibraryViewController.swift
@@ -13,25 +13,14 @@ import RealmSwift
 
 @available(iOS 13.0, *)
 class LibraryViewController: UISplitViewController, UISplitViewControllerDelegate {
-    private let sidebarViewController: UIViewController
+    private let sidebarViewController: UIHostingController<LibrarySidebarView>
     private let sidebarNavigationViewController: UINavigationController
     
     init() {
-        let sidebarView = LibrarySidebarView()
-        let sidebarViewController = UIHostingController(rootView: sidebarView)
-        self.sidebarViewController = sidebarViewController
+        self.sidebarViewController = UIHostingController(rootView: LibrarySidebarView())
         self.sidebarNavigationViewController = UINavigationController(rootViewController: sidebarViewController)
-        
         self.sidebarNavigationViewController.navigationBar.prefersLargeTitles = true
 
-//        super.init(style: .doubleColumn)
-//        preferredDisplayMode = .oneBesideSecondary
-//        preferredSplitBehavior = .tile
-//        presentsWithGesture = false
-        
-//        setViewController(self.sidebarViewController, for: .primary)
-//        setViewController(UIHostingController(rootView: LibraryCategoryView(category: .wikipedia)), for: .secondary)
-        
         super.init(nibName: nil, bundle: nil)
         preferredDisplayMode = .allVisible
         viewControllers = [sidebarNavigationViewController]
@@ -45,11 +34,17 @@ class LibraryViewController: UISplitViewController, UISplitViewControllerDelegat
             UIBarButtonItem(barButtonSystemItem: .add, target: nil, action: nil),
             UIBarButtonItem(image: UIImage(systemName: "info.circle"), style: .plain, target: nil, action: nil),
         ]
-        showCategory(.wikipedia)
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        if traitCollection.horizontalSizeClass == .regular {
+            showCategory(.wikipedia)
+        }
     }
     
     @objc func dismissController() {
@@ -58,12 +53,6 @@ class LibraryViewController: UISplitViewController, UISplitViewControllerDelegat
     
     @objc func openInfoController() {
         
-    }
-    
-    func splitViewController(_ splitViewController: UISplitViewController,
-                             collapseSecondary secondaryViewController: UIViewController,
-                             onto primaryViewController: UIViewController) -> Bool {
-        true
     }
     
     func showCategory(_ category: ZimFile.Category) {
