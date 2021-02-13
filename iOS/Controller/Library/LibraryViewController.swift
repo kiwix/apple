233d29836329f56d253ccf9670555dfad885c77a
Chris Li <chris@kiwix.org>
2021-02-13 15:26:34 -0500

    refactoring

--- iOS/Controller/Library/LibraryViewController.swift
+++ iOS/Controller/Library/LibraryViewController.swift
@@ -9,6 +9,8 @@
 import Combine
 import SwiftUI
 import UIKit
+
+import Defaults
 import RealmSwift
 
 @available(iOS 13.0, *)
@@ -209,5 +211,62 @@ private class ViewModel: ObservableObject {
     func fetchOnlineCatalog() {
         let operation = OPDSRefreshOperation()
         LibraryOperationQueue.shared.addOperation(operation)
+    }
+}
+
+
+@available(iOS 13.0, *)
+struct LibraryInfoView: View {
+    @Default(.libraryLastRefreshTime) private var lastRefreshTime
+    @Default(.libraryAutoRefresh) private var isAutoRefreshEnabled
+    @Default(.backupDocumentDirectory) private var isBackingUpDocumentDirectory
+    @Default(.libraryFilterLanguageCodes) private var languageCodes
+    
+    var dismiss: (() -> Void) = {}
+    
+    var body: some View {
+        NavigationView {
+            List {
+                Section(
+                    header: Text("Update"),
+                    footer: Text(
+                        """
+                        With auto update enabled, the catalog will be updated both when library \
+                        is opened and utilizing iOS's Background App Refresh feature.
+                        """
+                    )
+                ) {
+                    HStack {
+                        Text("Last updated")
+                        Spacer()
+                        lastUpdatedTime.foregroundColor(.secondary)
+                    }
+                    Toggle("Auto update", isOn: $isAutoRefreshEnabled)
+                    HStack {
+                        Spacer()
+                        Button("Update now") { }
+                        Spacer()
+                    }
+                }
+                Section(header: Text("Backup"), footer: Text("Does not apply to files that were opened in place.")) {
+                    Toggle("Include files in backup", isOn: $isBackingUpDocumentDirectory)
+                }
+            }
+            .insetGroupedListStyle()
+            .navigationBarTitle("Info", displayMode: .inline)
+            .navigationBarItems(leading: Button("Done", action: dismiss))
+        }.navigationViewStyle(StackNavigationViewStyle())
+    }
+    
+    var lastUpdatedTime: some View {
+        if let refreshTime = Defaults[.libraryLastRefreshTime] {
+            if Date().timeIntervalSince(refreshTime) < 120 {
+                return Text("Just now")
+            } else {
+                return Text(RelativeDateTimeFormatter().localizedString(for: refreshTime, relativeTo: Date()))
+            }
+        } else {
+            return Text("Never")
+        }
     }
 }
